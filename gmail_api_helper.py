8432a447baa7b77e6cf99e84cf5b38ec2370eff4
Daniel Caballero <danielcaballero88@gmail.com>
2022-11-26 17:54:37 +0700

    Add helper module for the gmail api

---
+++ gmail_api_helper.py
@@ -0,0 +1,71 @@
+"""Helper module for the GMAIL API functions."""
+import base64
+import os
+from email.message import EmailMessage
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+HERE = os.path.dirname(__file__)
+
+# https://developers.google.com/gmail/api/auth/scopes
+SCOPES = [
+    # "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.compose",
+]
+
+
+def gmail_create_and_send_draft(subject, content):
+    """Create a gmail draft and send it."""
+    # Authentication
+    creds = None
+    token_file_path = os.path.join(HERE, "token.json")
+    credentials_file_path = os.path.join(HERE, "credentials.json")
+    if os.path.exists(token_file_path):
+        creds = Credentials.from_authorized_user_file(token_file_path, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credentials_file_path, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_file_path, "w", encoding="utf-8") as token:
+            token.write(creds.to_json())
+
+    # Create and send draft.
+    try:
+        # create gmail api client
+        service = build("gmail", "v1", credentials=creds)
+
+        message = EmailMessage()
+
+        message["To"] = "danielcaballero88@gmail.com"
+        message["From"] = "danielcaballero88@gmail.com"
+        message["Subject"] = subject
+        message.set_content(content)
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+        create_message = {"message": {"raw": encoded_message}}
+        # pylint: disable=E1101
+        draft = (
+            service.users().drafts().create(userId="me", body=create_message).execute()
+        )
+
+        print(f'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
+
+        service.users().drafts().send(userId="me", body={"id": draft["id"]}).execute()
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        draft = None
+
+    return draft
