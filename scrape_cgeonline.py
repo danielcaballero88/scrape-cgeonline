992af7d96d37546ba6e29f83c4684cc793cc7b3e
Daniel Caballero <danielcaballero88@gmail.com>
2022-11-26 18:37:15 +0700

    Add notifications via email and logging

--- scrape_cgeonline.py
+++ scrape_cgeonline.py
@@ -1,13 +1,32 @@
 """Python script to scrape cgeonline."""
 
+import argparse
+import logging
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 from dc_logging import get_logger
 
+from gmail_api_helper import gmail_create_and_send_draft
+
 CGEONLINE_URL = "https://www.cgeonline.com.ar"
 DATES_URL = "/informacion/apertura-de-citas.html"
 
-logger = get_logger({"file_name": "scrape-cgeonline.log"})
+# Root logger defines the file and level for any log propagating up
+# the hierarchy. Useful to catch 3rd party libraries warnings and error
+# logs.
+root_logger = get_logger(
+    {
+        "file_name": "scrape_cgeonline.log",
+        "level": logging.WARNING,
+    }
+)
+
+# The scraper logger is set up independently so any logging is done
+# here is not duplicated by the root logger (propagate = False).
+logger = get_logger({"name": "scraper", "file_name": "scrape_cgeonline.log"})
+logger.propagate = False
 
 
 def scrape_cgeonline_dates_page():
@@ -64,9 +83,44 @@ def scrape_cgeonline_dates_page():
     return result
 
 
+def parse_arguments():
+    """Parse passed arguments and return args Namespace."""
+    parser = argparse.ArgumentParser("")
+    parser.add_argument(
+        "--email-every-time",
+        dest="email_every_time",
+        action="store_true",
+        help="Sends email for every execution even it there is no new info.",
+    )
+    parsed_args = parser.parse_args()
+    return parsed_args
+
+
 if __name__ == "__main__":
+    args = parse_arguments()
+
     try:
         row_data = scrape_cgeonline_dates_page()
-        logger.info(row_data)
     except Exception as exc:
         logger.error(exc)
+        gmail_create_and_send_draft(
+            subject="Error scraping cgeonline",
+            content=str(exc) + "\n\n" + CGEONLINE_URL + DATES_URL,
+        )
+        sys.exit(1)
+    else:
+        logger.info(row_data)
+
+        if row_data["proxima_apertura"] == "fecha por confirmar":
+            logger.info("No new date: %s")
+            if args.email_every_time:
+                gmail_create_and_send_draft(
+                    subject="No new date in cgeonline.",
+                    content=str(row_data) + "\n\n" + CGEONLINE_URL + DATES_URL,
+                )
+        else:
+            logger.info("New info: %s", row_data)
+            gmail_create_and_send_draft(
+                subject="New date in cgeonline!",
+                content=str(row_data) + "\n\n" + CGEONLINE_URL + DATES_URL,
+            )
