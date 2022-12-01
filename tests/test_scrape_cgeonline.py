42e0233fead7dee99edfdd2e5c160e874a253885
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-01 19:47:54 +0700

    Improve tests
      Added html static data for 3 test cases:
      - no_changes (current data)
      - error (changes in the structure that cause a scraping error)
      - new_data (new data in the target cell)
    
      For now only the test for no_changes was present, and it is improved
      to check that the correct data is extracted.

--- tests/test_scrape_cgeonline.py
+++ tests/test_scrape_cgeonline.py
@@ -1,33 +1,52 @@
 """Tests for the cgeonline scraper."""
+import os
+
 import pytest  # pylint: disable=unused-import
 from pytest_mock.plugin import MockerFixture
+
 from src import scrape_cgeonline
-import os
 
 # pylint: disable=protected-access
 
 HERE = os.path.dirname(__file__)
+DATA_DIR = os.path.join(HERE, "data")
+
 
 class MockGetResponse:
     """Mock response object, reading html from static file."""
+
+    def __init__(self, case: str):
+        self.case = case
+        response_filename = {
+            "no_changes": "test_response_no_changes.html",
+            "error": "test_response_error.html",
+            "new_date": "test_response_new_date.html",
+        }[case]
+        self.response_filepath = os.path.join(DATA_DIR, response_filename)
+
     @property
     def text(self):
         """Mocks requests.response.text."""
         test_response_html = None
         with open(
-            file=os.path.join(HERE, "test_response.html"),
-            mode="r",
-            encoding="utf-8"
+            file=self.response_filepath, mode="r", encoding="utf-8"
         ) as test_response_file:
             test_response_html = test_response_file.read()
         return test_response_html
 
 
-def test_scrape_cgeonline_dates_page(mocker: MockerFixture):
-    """Unit test for a single function."""
-    mock_get_response = mocker.Mock(return_value=MockGetResponse())
+def test_scrape_cgeonline_dates_page_no_changes(mocker: MockerFixture):
+    """Unit test for a scraping cgeonline dates when no changes."""
+    mock_get_response = mocker.Mock(return_value=MockGetResponse("no_changes"))
     mocker.patch("requests.get", mock_get_response)
 
     scraped_row = scrape_cgeonline._scrape_cgeonline_dates_page()
 
-    print(scraped_row)
+    assert isinstance(scraped_row, dict)
+
+    assert scraped_row["servicio"] == "Registro Civil-Nacimientos"
+    assert scraped_row["ultima_apertura"] == "10/11/2022"
+    assert scraped_row["proxima_apertura"] == "fecha por confirmar"
+    assert isinstance(scraped_row["solicitud"], str)
+    assert scraped_row["solicitud"].startswith("/")
+    assert scraped_row["solicitud"].endswith(".html")
