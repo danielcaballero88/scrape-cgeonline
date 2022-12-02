0305418b53190e14e0e2fc60ebb4aab3fdba920d
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-02 10:45:22 +0700

    Add test for error during scraping

--- tests/test_scrape_cgeonline.py
+++ tests/test_scrape_cgeonline.py
@@ -50,3 +50,12 @@ def test_scrape_cgeonline_dates_page_no_changes(mocker: MockerFixture):
     assert isinstance(scraped_row["solicitud"], str)
     assert scraped_row["solicitud"].startswith("/")
     assert scraped_row["solicitud"].endswith(".html")
+
+
+def test_scrape_cgeonline_dates_page_error(mocker: MockerFixture):
+    """Unit test for scraping cgeonline dates with an error."""
+    mock_get_response = mocker.Mock(return_value=MockGetResponse("error"))
+    mocker.patch("requests.get", mock_get_response)
+
+    with pytest.raises(ValueError):
+        scrape_cgeonline._scrape_cgeonline_dates_page()
